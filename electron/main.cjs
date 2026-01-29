d360ec00a62006468b4690e88fe1fba7ff17cf44
EAlZZZl <lpwcnm@outlook.com>
2026-01-29 22:46:28 +0800

    Special Version 1

--- electron/main.cjs
+++ electron/main.cjs
@@ -1,38 +1,45 @@
-// electron/main.cjs
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1200,
+    width: 1280,
     height: 800,
     webPreferences: {
       nodeIntegration: true,
+      contextIsolation: false,
     },
-    icon: path.join(__dirname, '../public/vite.svg') // 假设你有图标
+    // 如果有图标，可以取消注释下面这行并确保路径正确
+    // icon: path.join(__dirname, '../dist/vite.svg') 
   });
 
-  // 生产环境加载打包后的文件，开发环境加载 localhost
-  if (app.isPackaged) {
-    win.loadFile(path.join(__dirname, '../dist/index.html'));
-  } else {
+  // 开发环境下加载 Vite 服务，生产环境下加载打包好的文件
+  // 注意：我们在 package.json 的 electron:dev 命令中使用了 wait-on 来确保端口就绪
+  const isDev = !app.isPackaged;
+
+  if (isDev) {
     win.loadURL('http://localhost:5173');
+    // 开发模式下打开开发者工具
+    win.webContents.openDevTools();
+  } else {
+    // 生产环境加载 dist/index.html
+    // 使用 loadFile 加载本地文件
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
   }
-  
-  // 移除菜单栏（可选）
-  win.setMenuBarVisibility(false);
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
-  }
-});
-
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
   }
 });
