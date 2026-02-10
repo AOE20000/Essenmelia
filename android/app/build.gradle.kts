c2eb41ce5ad084afc7678b5b4ebccb686ef1bb25
EAlZZZl <lpwcnm@outlook.com>
2026-02-10 19:50:11 +0800

    添加图标

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.essenmelia_flutter"
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
